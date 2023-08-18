2fbe2945cbac4d19931f3152025ee9ab9a7c3124
BP20220606 <130937766+BP20220606@users.noreply.github.com>
2023-08-18 03:03:46 -0400

    Add files via upload

---
+++ Models/Alumno.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CRUD.Models;
+
+public partial class Alumno
+{
+    public int Id { get; set; }
+
+    public string Nombre { get; set; } = null!;
+
+    public string Apellido { get; set; } = null!;
+
+    public string? Telefono { get; set; }
+
+    public string? Correo { get; set; }
+
+    public string Foto { get; set; }
+}
---
+++ Models/CrudContext.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUD.Models;
+
+public partial class CrudContext : DbContext
+{
+    public CrudContext()
+    {
+    }
+
+    public CrudContext(DbContextOptions<CrudContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Alumno> Alumnos { get; set; }
+
+   // protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+     
+        
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+  //      => optionsBuilder.UseSqlServer("server=LAPTOP-7QMR7HQ9\\SQLEXPRESS02; database=CRUD; integrated security=true; TrustServerCertificate=True");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Alumno>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Alumnos__3214EC27CEA3215F");
+
+            entity.Property(e => e.Id).HasColumnName("ID");
+            entity.Property(e => e.Apellido)
+                .HasMaxLength(40)
+                .IsUnicode(false);
+            entity.Property(e => e.Correo)
+                .HasMaxLength(40)
+                .IsUnicode(false);
+            entity.Property(e => e.Foto)
+                .HasMaxLength(300)
+                .IsUnicode(false);
+            entity.Property(e => e.Nombre)
+                .HasMaxLength(40)
+                .IsUnicode(false);
+            entity.Property(e => e.Telefono)
+                .HasMaxLength(40)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
